fc3fd17c5b62f7fad912fec854970a3844fdcd89
Ahmed Bule <ahmedbule01@gmail.com>
2020-07-11 11:38:57 -0500

    restructured the files and created a countryflags html page

--- countryflags/index.js
+++
@@ -1,5 +0,0 @@
-import COUNTRIES from './countries.json';
-export default function hasFlag(country) {
-    // We are going to retun every country that we have in our JSON file. 
-return COUNTRIES.indexOf(country) >= 0;
-}
